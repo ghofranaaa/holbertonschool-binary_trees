829f1adadbd4147b0384368c3b1c2b4c3427b847
ghofranaaa <smailikhaled841@gmail.com>
2024-04-10 14:24:04 +0100

    checking if the tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -3,28 +3,57 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect- a function that checks if a binary tree is perfect.
- * @tree: pointer to the root node of the tree to check.
- * Return: 0 if tree if NULL.
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: Pointer to the root node of the tree to measure.
+ *
+ * Return: Height of the tree, 0 if tree is NULL.
  */
-
-int binary_tree_is_perfect(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_depth, right_depth;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	if (tree->left == NULL || tree->right == NULL)
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect.
+ * @tree: Pointer to the root node of the tree to check.
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise or if tree is NULL.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
 		return (0);
 
-	left_depth = binary_tree_height(tree->left);
-	right_depth = binary_tree_height(tree->right);
+	size_t left_height, right_height;
+	int is_perfect = 1;
 
-	return (left_depth == right_depth &&
-			binary_tree_is_perfect(tree->left) &&
-			binary_tree_is_perfect(tree->right));
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		if (left_height != right_height)
+			return (0);
+
+		is_perfect = binary_tree_is_perfect(tree->left) &&
+					 binary_tree_is_perfect(tree->right);
+	}
+	else if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (is_perfect);
 }
