64e74a781f3b92494c269a90e8822ac8a0d19031
ghofranaaa <smailikhaled841@gmail.com>
2024-04-17 12:21:57 +0100

    merging

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,29 +2,23 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
-/**
- * binary_tree_height- a function that checks if a node is a leaf.
- * @tree: pointer to the root node of the tree to measure the height.
- * Return: 0 if tree if NULL.
- */
+#include "binary_trees.h"
 
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: Pointer to the root node of the tree to measure.
+ *
+ * Return: Height of the tree, 0 if tree is NULL.
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height;
-	size_t right_height;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	if (left_height > right_height)
-	{
-		return (left_height + 1);
-	}
-	else
-
-		return (right_height + 1);
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
